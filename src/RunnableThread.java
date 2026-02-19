5340ac2bb633889dfb601414a579bc969c43ebe9
ARJUN M <arjunmurali091@gmail.com>
2026-02-19 23:44:01 +0530

    code submitted

--- src/RunnableThread.java
+++ src/RunnableThread.java
@@ -1,6 +1,19 @@
 class MyRunnable implements Runnable {
     // TODO: Override run() method
     // Loop from 1 to 5 and print numbers separated by space
+
+	@Override
+	public void run(){
+
+		for(int i=1;i<=5;i++){
+
+			System.out.print(i);
+			if(i!=5)System.out.print(" ");
+
+		}
+
+	}
+
 }
 
 public class RunnableThread {
@@ -8,5 +21,9 @@ public class RunnableThread {
         // TODO: Create an instance of MyRunnable
         // TODO: Create a Thread object passing the runnable instance
         // TODO: Start the thread
+
+	MyRunnable a= new MyRunnable();
+	Thread th= new Thread(a);
+	th.start();
     }
 }
